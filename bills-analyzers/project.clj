673b582c852166eabd5c58c3436d9061c134675d
ravithokala <t.ravisankar@gmail.com>
2013-01-05 02:07:35 +0000

    Merged phone analyzers and utility to create a common project for all bill-analyzers

--- bills-analyzers/project.clj
+++ bills-analyzers/project.clj
@@ -1,7 +1,8 @@
-(defproject utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject bills-analyzers "0.1.0-SNAPSHOT"
+  :description "Different bills Analyzers"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.csv "0.1.2"]])
+                 [org.clojure/data.csv "0.1.2"]
+                 ])
