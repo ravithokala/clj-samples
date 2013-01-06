b08b6890977de7179a68b463ec2efc27cda568e5
ravithokala <t.ravisankar@gmail.com>
2013-01-06 12:27:55 +0000

    Added utility function for rounding a number to required number of decimal places

--- bills-analyzers/src/utils/math.clj
+++ bills-analyzers/src/utils/math.clj
@@ -1,7 +1,13 @@
-(ns utils.math)
+(ns utils.math
+  (:use clojure.contrib.math)
+  )
 
 (defn double-digit-format
   "Prefixs zero in the front if n is less than 10"
   [n]
   (if (< n 10) (str "0" n) (str n)))
 
+(defn round-places [number decimals]
+  "Rounds a number to given number of decimals"
+  (let [factor (expt 10 decimals)]
+    (double (/ (round (* factor number)) factor))))
--- bills-analyzers/test/utils/math_test.clj
+++ bills-analyzers/test/utils/math_test.clj
@@ -13,3 +13,12 @@
     (testing "more than 2 digt"
       (is (= "567" (double-digit-format 567))))))
 
+(deftest testing-rounding
+  (testing "all-cases"
+    (testing "when decimal number is less than 5"
+      (is (= 99.78 (round-places 99.7812324 2))))
+    (testing "when decimal number is equal to 5"
+      (is (= 99.79 (round-places 99.785 2))))
+    (testing "when decimal number is greater than 5"
+      (is (= 99.79 (round-places 99.7852324 2)))))
+  )
