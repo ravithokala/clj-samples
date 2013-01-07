40cfd0c46d37e0c0612bf7f4781a8ebc3827aaf7
ravithokala <t.ravisankar@gmail.com>
2013-01-07 20:58:32 +0000

    Added functionality for extracting records for icici shares trades report

--- bills-analyzers/src/tradesbill/analyzer.clj
+++ bills-analyzers/src/tradesbill/analyzer.clj
@@ -2,10 +2,10 @@
   (:use [utils.data]))
 
 
-(defn get-all-transactions [filename map-fn]
+(defn get-all-transactions [filename mapping-fn]
   "Returns the transations for the transactions csv file and given mapping function"
   (let [l (take-csv filename)]
-    (map map-fn (rest l))))
+    (map mapping-fn (rest l))))
 
 (defn- map-icici-mf-record [record]
   "Converts the icici record to a map with appropriate keys"
@@ -13,11 +13,27 @@
    :transaction (if (= "Purchase" (record 2)) "Buy" "Sell")
    :name (record 4)
    :price (Double. (record 5))
-   :quantity (Double. (record 9))})
+   :quantity (Double. (record 9))
+   :brokerage 0.0})
 
 (defn get-all-icici-mfs-transactions [filename]
   "Returns the transations for the icici mfs transactions csv file"
   (get-all-transactions filename map-icici-mf-record))
+
+(defn- map-icici-share-record [record]
+  "Converts the icici record to a map with appropriate keys"
+  {:date (record 0)
+   :name (record 1)
+   :transaction (record 2)
+   :quantity (Double. (record 3))
+   :price (Double. (record 4))
+   :brokerage (Double. (record 6))
+   })
+
+(defn get-all-icici-shares-transactions [filename]
+  "Returns the transations for the icici shares transactions csv file"
+  (get-all-transactions filename map-icici-share-record))
+
 
 (defn- get-total-quantity [txs]
   "Returns the total quantity of the given transactions"
--- bills-analyzers/test/tradesbill/analyzer_test.clj
+++ bills-analyzers/test/tradesbill/analyzer_test.clj
@@ -4,8 +4,9 @@
         tradesbill.analyzer))
 
 (def icici-mf-sample (get-abs-path "tradesbill/sample_icici_mfs_report.csv"))
+(def icici-shares-sample (get-abs-path "tradesbill/sample_icici_shares_report.csv"))
 
-(deftest testing-csv-parsing-icici-reports
+(deftest testing-csv-parsing-icici-mf-reports
   (let [txs (get-all-icici-mfs-transactions icici-mf-sample)
         first-tx (first txs)
         second-tx (second txs)]
@@ -15,8 +16,24 @@
         "XYZ TAX GAIN SCHEME - DIVIDEND" (:name first-tx)
         50.0 (:price first-tx)
         500.0 (:quantity first-tx)
+        0.0 (:brokerage first-tx)
         "Buy" (:transaction first-tx)
         "Sell" (:transaction second-tx)
+        ))))
+
+(deftest testing-csv-parsing-icici-shares-reports
+  (let [txs (get-all-icici-shares-transactions icici-shares-sample)
+        first-tx (first txs)
+        second-tx (second txs)]
+    (testing "testing-date-purchase-name-price-quantity-brokerage"
+      (are [x y] (= x y)
+        "14-Oct-2010" (:date first-tx)
+        "DQENTE" (:name first-tx)
+        122.00 (:price first-tx)
+        80.0 (:quantity first-tx)
+        94.25 (:brokerage first-tx)
+        "Sell" (:transaction first-tx)
+        "Buy" (:transaction second-tx)
         ))))
 
 (deftest testing-calucation
